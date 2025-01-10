09d598d194a09de659461ddba3b9d2d4468c6626
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2025-01-10 15:28:25 +0100

    updated HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import getApiOptions from "../../services/api.js";
+
+import usePagination from "../../hooks/usePagination.js";
+
+import css from "./HomePage.module.css";
+
+import MovieList from "../../components/MovieList/MovieList.jsx";
+import Pagination from "../../components/Pagination/Pagination.jsx";
+
+const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  const [ttlPages, setTtlPage] = useState(0);
+
+  const savedPage = JSON.parse(sessionStorage.getItem("page")) || 1;
+
+  const { curPage, handleNextPage, handlePrevPage } = usePagination(
+    savedPage,
+    ttlPages
+  );
+
+  const url = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&page=${curPage}`;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(url, getApiOptions);
+        setMovies(response.data.results);
+        setTtlPage(response.data.total_pages);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, [curPage]);
+
+  return (
+    <section className="section">
+      <h1 className={css.homeTitle}>Trending Movies</h1>
+      <MovieList movies={movies} />
+      <Pagination
+        curPage={curPage}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        ttlPages={ttlPages}
+      />
+    </section>
+  );
+};
+
+export default HomePage;
